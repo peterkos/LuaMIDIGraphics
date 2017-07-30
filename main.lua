3b53e7baad1300f574a94885b38e1165a94b7c4a
Peter Kos <pkos91@icloud.com>
2017-07-30 16:50:19 -0400

    Spaces -> Tabs
    What heathan chose Spaces, I do not know.

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 
 -- require("miditest")
+local inspect = require("inspect")
 local midiInput = require("MidiParser")
 io.stdout:setvbuf('no') -- Allows console printing on macOS
 
@@ -10,23 +11,24 @@ function love.load()
 	love.graphics.setColor(0, 0, 0)
 
 	-- Load a MIDI file
-    midiInput.new("miditest")
-    midiInput.printNotes()
+	midiInput.new("miditest")
+	midiInput.printNotes()
 
 
 end
 
 -- Property to prevent redrawing of notes
 local notesWereDrawn = false
+local noteCount = 1
 
 function love.draw()
 
-    -- Draw a debugging grid
-    love.graphics.setColor(200, 0, 0, 100)
-    -- drawGrid()
+	-- Draw a debugging grid
+	love.graphics.setColor(200, 0, 0, 100)
+	-- drawGrid()
 
-    love.graphics.setColor(0, 200, 0)
-    love.graphics.print("Hello World", 400, 300)
+	love.graphics.setColor(0, 200, 0)
+	love.graphics.print("Hello World", 400, 300)
 
     -- Does this only once
     if notesWereDrawn == false then
@@ -44,32 +46,32 @@ end
 -- Draw a grid for debugging pourposes. Purpoises. Porcupines!
 function drawGrid()
 
- 	-- Global values
- 	inc = 33
+	-- Global values
+	inc = 33
 	-- love.graphics.setNewFont(font)
 	windowHeight = love.graphics.getHeight()
 	windowWidth = love.graphics.getWidth()
 	
 
-    -- Initialize parameters
-    x1 = 0
-    x2 = 0
-    y1 = 0
-    y2 = 0
+	-- Initialize parameters
+	x1 = 0
+	x2 = 0
+	y1 = 0
+	y2 = 0
 
-    -- Draw vertical
-    for i = 0, windowWidth / 10, 1 do
-    	love.graphics.line(x1, 0, x2, windowHeight)
-    	x1 = x1 + inc
-    	x2 = x2 + inc
-    end
+	-- Draw vertical
+	for i = 0, windowWidth / 10, 1 do
+		love.graphics.line(x1, 0, x2, windowHeight)
+		x1 = x1 + inc
+		x2 = x2 + inc
+	end
 
-    -- Draw horizontal
-    for i = 0, windowHeight / 10, 1 do
-    	love.graphics.line(0, y1, windowWidth, y2)
-    	y1 = y1 + inc
-    	y2 = y2 + inc
-    end
+	-- Draw horizontal
+	for i = 0, windowHeight / 10, 1 do
+		love.graphics.line(0, y1, windowWidth, y2)
+		y1 = y1 + inc
+		y2 = y2 + inc
+	end
 
-    -- Print coordinates
+	-- Print coordinates
 end
