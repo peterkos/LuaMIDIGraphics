a0a039a241000433f48b4416a83dddd54cbf7527
Peter Kos <pkos91@icloud.com>
2017-07-30 16:18:55 -0400

    Made scoreNotes table externally accessable

--- MidiParser.lua
+++ MidiParser.lua
@@ -2,7 +2,7 @@
 local MIDI = require("MIDI")
 
 score = {}
-scoreNotes = {}
+score.scoreNotes = {}
 local track = 2
 local removeNonNotes
 
@@ -10,14 +10,14 @@ function score.new(filename)
 
 	-- Load score from file
 	local midiFile = io.open(filename .. ".mid", "r")
-	scoreNotes = MIDI.midi2score(midiFile:read("*a"))
+	score.scoreNotes = MIDI.midi2score(midiFile:read("*a"))
 	midiFile:close()
 
 	-- Sort by note start time rather than note end
 	-- (midi2score() sorts by note end)
-	table.sort(scoreNotes[2], function (e1, e2) return e1[2] < e2[2] end)
+	table.sort(score.scoreNotes[2], function (e1, e2) return e1[2] < e2[2] end)
 
-	scoreNotes = removeNonNotes()
+	score.scoreNotes = removeNonNotes()
 end
 
 
@@ -27,7 +27,7 @@ function score.printNotes()
 	
 	io.write("[type, start, duration, chan, note, velocity]\n")
 
-	for k, event in ipairs(scoreNotes[2]) do
+	for k, event in ipairs(score.scoreNotes[2]) do
 		io.write(string.format("%d %5s, %5s, %5s, %4s, %5s, %6s\n", k, event[1], event[2], event[3], event[4], event[5], event[6]))
 	end
 
@@ -40,19 +40,19 @@ removeNonNotes = function()
 	removed = 0
 
 	local i = 1
-	for i=#scoreNotes[track], 1, -1 do
-		if (scoreNotes[track][i][1] == "note") then 
-			io.write(string.format("Kept %s %d\n", scoreNotes[track][i][1], i))
+	for i=#score.scoreNotes[track], 1, -1 do
+		if (score.scoreNotes[track][i][1] == "note") then 
+			io.write(string.format("Kept %s %d\n", score.scoreNotes[track][i][1], i))
 			i = i + 1
 		else
-			io.write(string.format("Removing  %d %s\n", i, scoreNotes[track][i][1]))
-			table.remove(scoreNotes[track], i)
+			io.write(string.format("Removing  %d %s\n", i, score.scoreNotes[track][i][1]))
+			table.remove(score.scoreNotes[track], i)
 			removed = removed + 1
 		end
 	end
 
 	io.write(string.format("\nRemoved %d events\n-----\n\n", removed))
-	return scoreNotes
+	return score.scoreNotes
 	
 
 end
