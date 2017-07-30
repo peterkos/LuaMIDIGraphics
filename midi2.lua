b3aeed7183a691cf4dc7977af10716a0438c0df8
Peter Kos <pkos91@icloud.com>
2017-07-30 14:01:44 -0400

    Distinguished MIDI helper object from notes table
    Originally score{} held the table of MIDI notes as well as the functions of this “class” (because that’s how Lua works), but now score{} holds all the functions and scoreNotes{} holds all the actual MIDI data.

--- midi2.lua
+++ midi2.lua
@@ -2,54 +2,59 @@
 local MIDI = require("MIDI")
 
 score = {}
+scoreNotes = {}
+local track = 2
+local removeNonNotes
 
-function new(filename)
+function score.new(filename)
 
 	-- Load score from file
 	local midiFile = io.open(filename .. ".mid", "r")
-	score = MIDI.midi2score(midiFile:read("*a"))
+	scoreNotes = MIDI.midi2score(midiFile:read("*a"))
 	midiFile:close()
 
 	-- Sort by note start time rather than note end
 	-- (midi2score() sorts by note end)
-	table.sort(score[2], function (e1, e2) return e1[2] < e2[2] end)
+	table.sort(scoreNotes[2], function (e1, e2) return e1[2] < e2[2] end)
 
-	score = removeNonNotes()
+	scoreNotes = removeNonNotes()
 end
 
 
 -- Print out info of notes
-function printNotes() 
+-- Precondition: removeNonNotes() called before this
+function score.printNotes() 
 	
 	io.write("[type, start, duration, chan, note, velocity]\n")
 
-	for k, event in ipairs(bachScoreNotes[firstTrack]) do
+	for k, event in ipairs(scoreNotes[2]) do
 		io.write(string.format("%d %5s, %5s, %5s, %4s, %5s, %6s\n", k, event[1], event[2], event[3], event[4], event[5], event[6]))
 	end
 
 end
 
 
-
 -- Returns new score of only note events
-local function removeNonNotes()
+removeNonNotes = function()
 
 	removed = 0
 
 	local i = 1
-	for i=#score[firstTrack], 1, -1 do
-		if (score[firstTrack][i][1] == "note") then 
-			io.write(string.format("Kept %s %d\n", score[firstTrack][i][1], i))
+	for i=#scoreNotes[track], 1, -1 do
+		if (scoreNotes[track][i][1] == "note") then 
+			io.write(string.format("Kept %s %d\n", scoreNotes[track][i][1], i))
 			i = i + 1
 		else
-			io.write(string.format("Removing  %d %s\n", i, score[firstTrack][i][1]))
-			table.remove(score[firstTrack], i)
+			io.write(string.format("Removing  %d %s\n", i, scoreNotes[track][i][1]))
+			table.remove(scoreNotes[track], i)
 			removed = removed + 1
 		end
 	end
 
 	io.write(string.format("Removed %d", removed))
-	return score
+	return scoreNotes
 	
 
 end
+
+return score
