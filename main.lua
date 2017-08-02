87834586406d83dd2fd38835721bf88f1049c58b
Peter Kos <pkos91@icloud.com>
2017-08-02 00:02:23 -0400

    Cleaned up some comments & variables

--- main.lua
+++ main.lua
@@ -57,6 +57,8 @@ function love.draw()
 
 end
 
+
+-- Draw new notes, with delay from update(dt)
 function drawNotes(i)
 	local note = midiInput.scoreNotes[i]
 	print("[INFO] i is equal to " .. i)
@@ -70,12 +72,9 @@ end
 -- Draw a grid for debugging pourposes. Purpoises. Porcupines!
 function drawGrid()
 
-	-- Global values
-	inc = 33
-	-- love.graphics.setNewFont(font)
-	windowHeight = love.graphics.getHeight()
-	windowWidth = love.graphics.getWidth()
-	
+	local inc = 33
+	local windowHeight = love.graphics.getHeight()
+	local windowWidth = love.graphics.getWidth()
 
 	-- Initialize parameters
 	x1 = 0
@@ -83,14 +82,14 @@ function drawGrid()
 	y1 = 0
 	y2 = 0
 
-	-- Draw vertical
+	-- Draw vertical lines
 	for i = 0, windowWidth / 10, 1 do
 		love.graphics.line(x1, 0, x2, windowHeight)
 		x1 = x1 + inc
 		x2 = x2 + inc
 	end
 
-	-- Draw horizontal
+	-- Draw horizontal lines
 	for i = 0, windowHeight / 10, 1 do
 		love.graphics.line(0, y1, windowWidth, y2)
 		y1 = y1 + inc
